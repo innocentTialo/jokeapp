4e5b99f8e84b15526f4a0c9e92f985efa3a90ca2
InnocentTIALO <tialoinnocent@gmail.com>
2020-05-23 20:49:24 +0100

    creation of a bean using xml base configuration

---
+++ src/main/java/com/ddf/joke/config/ChuckConfiguration.java
@@ -0,0 +1,16 @@
+package com.ddf.joke.config;
+
+import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by InnocentTIALO on 5/23/2020.
+ */
+//@Configuration
+public class ChuckConfiguration {
+    @Bean
+    public ChuckNorrisQuotes getChuckNorrisQuotes(){
+        return new ChuckNorrisQuotes();
+    }
+}
